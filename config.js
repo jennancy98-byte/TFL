f7df3362d7268e0f6d9ed34b0a5146813315b9f5
jennancy98-byte <jennancy98@gmail.com>
2025-10-15 10:20:52 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,168 @@
+const config = {
+    "giveaway": {
+      "name": "THEFANLOUNGE",
+      "heroHeadline": "Win a <span class='highlight'>Exclusive Giveaway & Gift Package</span> with THEFANLOUNGE!",
+      "heroSubheadline": "Sign up now for your chance to win a one-of-a-kind prize and exclusive gift package, brought to you by one of your favorite celebrities. It’s free, easy, and only available for a limited time!",
+      "promotionDates": "Promotion runs from May 1, 2025 until December 31, 2025.",
+      "endDate": "2025-12-31T03:59:00.000Z",
+      "heroBackgroundImageUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/2d3322d2-1afe-448b-83c7-57b6eb3e9931/DSC00313.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466UYAVY6ZS%2F20250508%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250508T004637Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEMH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIQCRiSVExAsrPRjS%2B2WWVh4IOH49K9pAjJrQqhsb7fCCPwIgPWgLViNJ1pGWNRKwBEZ6LWQagcX9iE9XzbFmllET1S4q%2FwMIahAAGgw2Mzc0MjMxODM4MDUiDHmHX1wIKZ3426CvdircA9v%2FHJllLC6zMPdveigdmhf3HbQv8p6V36Z8cxCRHXken7SD8WaDLMSRI4OpzP4yUpV0In3uN2elLzpVR602oZ8rh6ewQheq9eWunGKObzwOdhEjmgy86kRkJ1VbiBT1WaEu%2FE%2BrhMcxwjCGkCfb3KNvFMv3XTilp%2BpE0HaxkL%2Fa5eS3IhdI%2Fl86kvbv3Edb7jdyQBF8KUafIN2%2F5Y7PQycIfodyAdy0weckj20dZ%2FTdzObCM0bubGd9YSDKYp1utzsjuOLORzrEFoUa42DYQOXcNctzu2K5YLpyYhc6s27l2%2FZctBFd7MY8UZ0yzQ%2FRKhO9M9J3D%2BG5lepLUoTAvmNZ1hM88MgBVgvEVTdqJgDY99IL6xiNebn5StxtAGuISxYuqVNkCYfjPDubvSkzneSjWgDQlVaXKZE15lbH7u5Br2tsPMy43gFqbfnjleTJadd2VVBS%2BJfhhH9TICNriS6mDku1SnVB4G1OBDRG49gV%2BhwFPlBYFBSMaWPvR5zvVtdP3%2BnHEU70aTWZslw8vP7L61pp%2FfVPUl0Tuo8QNDvnOyJscdN6igg4Rrd6YO1Fdn3%2FsLx0CJk%2FUcAcY1vCuAEQHJCxMVb5diIuUjsI4Y1gz73sREl5wbUZVMkCMM7378AGOqUBSt0XJlLKUCABAIVWhphMWdS9RjQLPPea%2FuO74Gc7Md2JxYHtbGAf%2F68wgP%2FnLAftrnVPSbahcnkYBtBdDliXOmGoXkU39MMrQLEek%2B%2FOBn4jUVSUhMYDwNLQJBV6TPYHCWrCsLa4L2w%2FhxbTL5eTrJ%2BR%2FG%2BVDjiLSc3rMjtm%2Bo7rxmWA%2BM8BYDToh7kt2W%2FyT8rpj%2BJFg4bJjOzcIec%2BY0%2FShEyJ&X-Amz-Signature=8cb37ee72ac1559939deb00986d979fc557990943a8cae39aba11da156305e9a&X-Amz-SignedHeaders=host&x-id=GetObject",
+      "heroCtaText": "Enter Now to Win!",
+      "entryFormCtaText": "Count Me In!",
+      "successModalHeaderText": "🎉 You're In! 🎉",
+      "successModalMainMessage": "Your entry for the <strong>THEFANLOUNGE Giveaway</strong> has been successfully submitted. Best of luck!",
+      "successModalEmailPrompt": "You'll be contacted via email. Keep an eye on your inbox!"
+    },
+    "prize": {
+      "images": [
+        {
+          "src": "https://img.freepik.com/free-vector/gradient-enter-win-label_23-2150306088.jpg",
+          "alt": "THEFANLOUNGE Giveaway Gift Package"
+        }        
+      ],
+      "includedItems": [
+        {
+          "icon": "fas fa-home",
+          "text": "Exclusive Giveaway Hosted by a Featured Celebrity"
+        },
+        {
+          "icon": "fas fa-gifts",
+          "text": "Curated Gift Package with Unique Items"
+        },
+        {
+          "icon": "fas fa-users",
+          "text": "Special Surprise from a Celebrity Guest"
+        },
+        {
+          "icon": "fas fa-check-circle",
+          "text": "No purchase necessary to enter or win"
+        },
+        {
+          "icon": "fas fa-bolt",
+          "text": "Quick and easy online entry!"
+        }
+      ],
+      "name": "Exclusive Giveaway & Gift Package",
+      "value": "Exclusive Offer",
+      "description": "Sign up now for your chance to win a one-of-a-kind prize and exclusive gift package, brought to you by one of your favorite celebrities. It’s free, easy, and only available for a limited time!",
+      "limitedTimeOfferText": "🎁THEFANLOUNGE Special Giveaway!",
+      "ctaButtonText": "I WANT TO WIN THIS!"
+    },
+    "howToEnter": {
+      "steps": [
+        {
+          "icon": "fas fa-keyboard",
+          "title": "Fill the Form",
+          "description": "Simply enter your name and email address in the form below. It takes less than 30 seconds!"
+        },
+        {
+          "icon": "fas fa-share-alt",
+          "title": "Share (Optional)",
+          "description": "Spread the word on social media using the links after you enter. (Sharing is caring, but not required for entry!)"
+        },
+        {
+          "icon": "fas fa-envelope-open-text",
+          "title": "Check Your Email",
+          "description": "We'll contact the winner via email. Make sure you enter a valid email address and check your spam folder too!"
+        }
+      ],
+      "highlights": [
+        {
+          "icon": "fas fa-stopwatch",
+          "text": "Quick & Easy Entry"
+        },
+        {
+          "icon": "fas fa-gift",
+          "text": "Exclusive Gift Package"
+        },
+        {
+          "icon": "fas fa-smile",
+          "text": "Free to Enter!"
+        }
+      ]
+    },
+    "entryForm": {
+      "sharePlatforms": {
+        "facebook": "https://www.facebook.com/share/1Ftd1RBfiP/?mibextid=wwXlfr",
+        "twitter": "https://twitter.com/intent/tweet?url=https%3A%2F%2Fgiveaway-294.netlify.app%2Fadmin&text=Win%20a%20Free%20Home%20Consultation%20with%20Erica%20Reddy-Choquette!"
+      },
+      "subtitle": "Enter your details below for a chance to win an exclusive prize!",
+      "entryCountText": "Join over 200+ entries already! Don't miss out!",
+      "socialSharePrompt": "Tell your friends about this amazing giveaway:",
+      "termsText": "By entering, you agree to our <a href='http://thefanlounge.site' target='_blank'>Privacy Policy</a>."
+    },
+    "rules": {
+      "faq": [
+        {
+          "q": "Who is eligible to enter?",
+          "a": "This giveaway is open to legal residents of the US and Canada who are 18 years of age or older at the time of entry."
+        },
+        {
+          "q": "How long does the giveaway run?",
+          "a": "The giveaway starts on May 1, 2025, and all entries must be received by Dec 31, 2025, at 11:59 PM EST."
+        },
+        {
+          "q": "How will the winner be selected?",
+          "a": "A Hundred (100) winners will be selected randomly from all eligible entries received during the promotion period."
+        },
+        {
+          "q": "When and how will the winner be notified?",
+          "a": "The winner will be notified by email on or before Dec 31, 2025. They will have 48 hours to claim their prize."
+        },
+        {
+          "q": "How many times can I enter?",
+          "a": "Limit one (1) entry per person/email address during the entire promotion period. Duplicate entries will be disqualified."
+        },
+        {
+          "q": "Is my information secure?",
+          "a": "We respect your privacy. Your information will be used solely for this giveaway and in accordance with our Privacy Policy. We do not sell your data."
+        }
+      ],
+      "tips": [
+        "Double-check your email address for typos before submitting.",
+        "Add management@thefanlounge.site to your address book to ensure you receive notifications.",
+        "Follow THEFANLOUNGE on social media for winner announcements."
+      ],
+      "fairSelectionInfo": {
+        "title": "Fair & Square Selection",
+        "text": "Our winner selection process is completely random and unbiased, ensuring everyone has an equal chance. The draw will be conducted using a certified random number generator."
+      },
+      "importantNotice": {
+        "title": "Winner Notification - Check Your Email!",
+        "text": "The lucky winner will be contacted exclusively via the email address provided during entry. Please ensure your email is correct. Check your inbox, promotions tab, and spam/junk folder regularly."
+      },
+      "fullRulesPdfLink": ""
+    },
+    "footerContact": {
+      "social": {
+        "facebook": "https://www.facebook.com/share/1Ftd1RBfiP/?mibextid=wwXlfr",
+        "instagram": "https://www.instagram.com/thefanlounge"
+      },
+      "organizerName": "THEFANLOUNGE Team",
+      "email": "management@thefanlounge.site",
+      "phone": "(323) 531-3371",
+      "address": "1300 Jericho Turnpike, Syosset, NY 11791",
+      "copyrightOwner": "THEFANLOUNGE Team"
+    },
+    "meta": {
+      "pageTitle": "THEFANLOUNGE 2025 Giveaway!",
+      "navBrandLogoText": "THEFANLOUNGE",
+      "privacyPolicyLink": "http://thefanlounge.site",
+      "termsLink": "http://thefanlounge.site"
+    },
+    "settings": {
+      "confettiColors": [
+        "#004aad",
+        "#f7a544",
+        "#009688",
+        "#ffffff"
+      ],
+      "showCountdownInHero": true
+    },
+    "deploymentInfo": {
+      "repoName": "THEFANLOUNGE",
+      "repoUrl": "https://thefanlounge.site",
+      "tag": "THEFANLOUNGE 2025",
+      "netlifyUrl": "https://thefanlounge.site",
+      "netlifyId": "979716955"
+    },
+    "modalQuestions": []
